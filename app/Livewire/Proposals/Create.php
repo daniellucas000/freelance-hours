683772cdfe7a865530e0d8f279e619194376ca34
Daniel Lucas <danielluccas000@gmail.com>
2024-10-14 00:56:35 -0300

    feat: position update and notifications

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -2,7 +2,12 @@
 
 namespace App\Livewire\Proposals;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
+use App\Models\Proposal;
+use App\Notifications\NewProposal;
+use App\Notifications\PerdeuMane;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
 
@@ -29,15 +34,40 @@ class Create extends Component
             return;
         }
 
-        $this->project->proposals()
-            ->updateOrCreate(
-                ['email' => $this->email],
-                ['hours' => $this->hours]
-            );
+        DB::transaction(function () {
+            $proposal = $this->project->proposals()
+                ->updateOrCreate(
+                    ['email' => $this->email],
+                    ['hours' => $this->hours]
+                );
+
+            $this->arrangePositions($proposal);
+        });
+
+        $this->project->author->notify(new NewProposal($this->project));
 
         $this->dispatch('proposal::created');
 
         $this->modal = false;
+    }
+
+    public function arrangePositions(Proposal $proposal)
+    {
+        $query = DB::select('
+            select *, row_number() over (order by hours asc) as newPosition
+            from proposals
+            where project_id = :project
+            ', ['project' => $proposal->project_id]);
+        $position = collect($query)->where('id', '=', $proposal->id)->first();
+        $otherProposal = collect($query)->where('position', '=', $position->newPosition)->first();
+        if ($otherProposal) {
+            $proposal->update(['position_status' => 'up']);
+            $oProposal = Proposal::find($otherProposal->id);
+
+            $oProposal->update(['position_status' => 'down']);
+            $oProposal->notify(new PerdeuMane($this->project));
+        }
+        ArrangePositions::run($proposal->project_id);
     }
 
     public function render()
